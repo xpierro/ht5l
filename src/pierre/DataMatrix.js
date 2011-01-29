4819c70934658d81abd3e6e83a9a50d729124f0a
xpierro <xpierro@gmail.com>
2011-01-29 15:33:51 +0000

    Implémentation d'une matrice permettant de décrire les données simplement, en vue de les dessiner.
    D'autres fonctions utiles seront implémentées au fur et à mesure.
    
    Chaque diagramme devra passer pas ce type de données (donc plus de double tableau, ou autre exotisme).

---
+++ src/pierre/DataMatrix.js
@@ -0,0 +1,83 @@
+/**
+ * Matrice des données représentant un tableau à double entrée, indexé en
+ * x et en y.
+ */
+
+var DataMatrix = function(w, h) {
+	var width = width;
+	var height = height;
+	
+	var xLabels = new Array();
+	var yLabels = new Array();
+	
+	var rows = new Array();
+	
+	/**
+	 * Retourne l'index correspondant à la valeur
+	 * @param value La valeur à retrouver
+	 * @returns L'index de la valeur
+	 */
+	var findValueInArray = function(array, value) {
+		var index = -1;
+		$.each(array, function(i, val) {
+			if (value == val) {
+				index = i;
+				return;
+			};
+		});
+		return index;
+	};
+	
+	/**
+	 * Spécifie le nom de chaque abscisse
+	 */
+	this.setXAxisLabels = function(labels) {
+		$.each(labels, function(index, value) {
+			xLabels.push(value);
+		});
+	};
+	
+	/**
+	 * Spécifie le nom de chaque ordonnée
+	 */	
+	this.setYAxisLabels = function(labels) {
+		$.each(labels, function(index, value) {
+			yLabels.push(value);
+		});
+	};
+	
+	/**
+	 * Insère la valeur value dans la matrices, aux coordonnées labélisées
+	 * fournies.
+	 */
+	this.setValue = function(xLabel, yLabel, value) {
+		var i = findValueInArray(xLabels, xLabel);
+		var j = findValueInArray(yLabels, yLabel);
+		if (i != -1 && j != -1) {
+			if (!rows[yLabel]) {
+				rows[yLabel] = new Array();
+			}
+			rows[yLabel][xLabel] = value;
+		} else {
+			throw "UndefinedLabel";
+		}
+	};
+	
+	/**
+	 * Retoune la valeur entrée dans la matrice selon les labels en x et y.
+	 */
+	this.getValueByLabel = function(xLabel, yLabel) {
+		if (!rows[yLabel] || !rows[yLabel][xLabel]) {
+			throw "UndefinedLabel";
+		}
+		return rows[yLabel][xLabel];
+	};
+	
+	/**
+	 * Retourne la valeur entrée dans la matrice selon ses coordonnées
+	 * numériques.
+	 */
+	this.getValue = function(x, y) {
+		return this.getValueByLabel(xLabels[x], yLabels[y]);
+	};
+};
