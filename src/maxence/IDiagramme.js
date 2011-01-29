9a1f75ea2f56ce562b8d4279f232cf262b498ec4
geglou <geglou@gmail.com>
2011-01-29 19:44:32 +0000

    MaJ IDiagramme

--- src/maxence/IDiagramme.js
+++ src/maxence/IDiagramme.js
@@ -1,46 +1,60 @@
-/*
-*	Définit la largeur de la fenêtre du diagramme
-*	Argument : 
-		- x : Nombre
-*/
-var setWindowWidth = function(x){};
+var IDiagramme = function() {
 
-/*
-*	Retourne la largeur de la fenêtre du diagramme
-*/
-var getWindowWidth = function(){};
-
-/*
-*	Définit la hauteur de la fenêtre du diagramme
-*	Argument :
-*		- y : Nombre
-*/
-var setWindowHeight = function(y){};
-
-/*
-*	Retourne la hauteur de la fenêtre du diagramme
-*/
-var getWindowHeight = function(){};
-
-/*
-*	Définit la couleur du fond du diagramme (à la limite on peut ajouter un argument pour définir la transparence)
-*	Argument : 
-*		- color : couleur désirée (codé en hexa?)
-*/
-var setBackgroundColor = function(color){};
-
-/*
-*	Retourne la couleur du fond du diagramme
-*/
-var getBackgroundColor = function(){};
-
-/*
-*	Dessine la légende du diagramme
-*	Argument :
-*		- Tableau : tableau a deux dimensions avec paires couleur/nom donnée
-*/
-var drawLegend = function(tableau){};
-
-/*
-*	
-*/
+	/*
+	*	Définit la largeur de la fenêtre du diagramme
+	*	Argument : 
+			- x : Nombre
+	*/
+	this.setWindowWidth = function(x){};
+	
+	/*
+	*	Retourne la largeur de la fenêtre du diagramme
+	*/
+	this.getWindowWidth = function(){};
+	
+	/*
+	*	Définit la hauteur de la fenêtre du diagramme
+	*	Argument :
+	*		- y : Nombre
+	*/
+	this.setWindowHeight = function(y){};
+	
+	/*
+	*	Retourne la hauteur de la fenêtre du diagramme
+	*/
+	this.getWindowHeight = function(){};
+	
+	/*
+	*	Définit la couleur du fond du diagramme (à la limite on peut ajouter un argument pour définir la transparence)
+	*	Argument : 
+	*		- color : couleur désirée (codé en hexa?)
+	*/
+	this.setBackgroundColor = function(color){};
+	
+	/*
+	*	Retourne la couleur du fond du diagramme
+	*/
+	this.getBackgroundColor = function(){};
+	
+	/*
+	*	Dessine la légende du diagramme
+	*	Argument :
+	*		- Tableau : tableau a deux dimensions avec paires couleur/nom donnée
+	*/
+	this.drawLegend = function(tableau){};
+	
+	/*
+	*	Dessine les abscisses et ordonnées des diagrammes ligne et histogrammes
+	*	Argument :
+	*		- Tableau : tableau a deux dimensions, une colonne pour les abscisses, l'autre pour les ordonnées
+	*/
+	this.drawAxis = function(tableau){};
+	
+	/*
+	*	Dessine le diagramme
+	*	Argument :
+	*		- Tableau : tableau a deux dimensions contenant nom donnée/valeur donnée/couleur par ligne 
+	*	(en gros un tableau de taille[x][3] où x est le nombre de données) 
+	*/
+	this.drawDiagram = function(tableau){};
+}
