7de9f147b6c70ffe0d18bb6999c0dbef74253189
geglou <geglou@gmail.com>
2011-01-30 14:25:55 +0000

    Ajout des fichiers exitants dans mon dossier et premier jet sur le PieDiragram.drawLegend

--- src/maxence/IDiagramme.js
+++ src/maxence/IDiagramme.js
@@ -1,60 +1,135 @@
-var IDiagramme = function() {
-
-	/*
-	*	Définit la largeur de la fenêtre du diagramme
-	*	Argument : 
-			- x : Nombre
-	*/
-	this.setWindowWidth = function(x){};
+var IDiagramme = function(canvasRef) {
+	this.canvas = canvasRef;
+	this.data = null;
 	
-	/*
-	*	Retourne la largeur de la fenêtre du diagramme
-	*/
-	this.getWindowWidth = function(){};
+	/**
+	 * Prototypage, toutes les methodes définies ici ne seront pas dupliquées
+	 * à l'instanciation.
+	 */
+    if (typeof IDiagramme.initialized == "undefined" ) {
+        IDiagramme.initialized = true;
+        
+		/**
+		 *	Définit la largeur de la fenêtre du diagramme.
+		 *	@param width Largeur de la fenêtre
+		 */
+		IDiagramme.prototype.setWidth = function(width){
+			this.canvas.setAttribute('width', width);
+			this.redraw();
+		};
 	
-	/*
-	*	Définit la hauteur de la fenêtre du diagramme
-	*	Argument :
-	*		- y : Nombre
-	*/
-	this.setWindowHeight = function(y){};
+		/**
+		 *	Retourne la largeur de la fenêtre du diagramme.
+		 */
+		IDiagramme.prototype.getWidth = function(){
+			return this.canvas.getAttribute('width');
+		};
 	
-	/*
-	*	Retourne la hauteur de la fenêtre du diagramme
-	*/
-	this.getWindowHeight = function(){};
+		/**
+		 *	Définit la hauteur de la fenêtre du diagramme
+		 *	@param height Hauteur de la fenêtre.
+		 */
+		IDiagramme.prototype.setHeight = function(height){
+			this.canvas.setAttribute('height', height);
+			this.redraw();
+		};
 	
-	/*
-	*	Définit la couleur du fond du diagramme (à la limite on peut ajouter un argument pour définir la transparence)
-	*	Argument : 
-	*		- color : couleur désirée (codé en hexa?)
-	*/
-	this.setBackgroundColor = function(color){};
+		/**
+		 *	Retourne la hauteur de la fenêtre du diagramme
+		 */
+		IDiagramme.prototype.getHeight = function(){
+			return this.canvas.getAttribute('height');
+		};
+		
+		/**
+		 * Charge un fichier de style pour le diagramme.
+		 * @param css Url d'un fichier css.
+		 */
+		IDiagramme.prototype.setStyle = function(css) {};
 	
-	/*
-	*	Retourne la couleur du fond du diagramme
-	*/
-	this.getBackgroundColor = function(){};
-	
-	/*
-	*	Dessine la légende du diagramme
-	*	Argument :
-	*		- Tableau : tableau a deux dimensions avec paires couleur/nom donnée
-	*/
-	this.drawLegend = function(tableau){};
-	
-	/*
-	*	Dessine les abscisses et ordonnées des diagrammes ligne et histogrammes
-	*	Argument :
-	*		- Tableau : tableau a deux dimensions, une colonne pour les abscisses, l'autre pour les ordonnées
-	*/
-	this.drawAxis = function(tableau){};
-	
-	/*
-	*	Dessine le diagramme
-	*	Argument :
-	*		- Tableau : tableau a deux dimensions contenant nom donnée/valeur donnée/couleur par ligne 
-	*	(en gros un tableau de taille[x][3] où x est le nombre de données) 
-	*/
-	this.drawDiagram = function(tableau){};
-}
+		/**
+		 * Charge les données du diagramme.
+		 * @param dataMatrix Matrice des données
+		 */
+		IDiagramme.prototype.setData = function(dataMatrix) {
+			this.data = dataMatrix;
+		};
+		
+		/**
+		 *	Dessine la légende du diagramme
+		 */
+		IDiagramme.prototype.drawLegend = function(){};
+		
+		/**
+		 * Dessine les abscisses et ordonnées du diagramme
+		 */
+		IDiagramme.prototype.drawAxis = function() {
+			this.drawXAxis();
+			this.drawYAxis();
+		};
+		
+		/**
+		 * Dessine les abscisses du diagramme
+		 */
+		IDiagramme.prototype.drawXAxis = function() {
+			var context = this.canvas.getContext('2d');
+			context.strokeStyle = "black";
+			context.beginPath();
+				// Ligne des abscisses
+				context.moveTo(50, this.getHeight() - 50);
+				context.lineTo(this.getWidth() - 50, this.getHeight() - 50);
+			context.closePath();
+			context.stroke();
+		};
+		
+		/**
+		 *	Dessine les ordonnées du diagramme
+		 */
+		IDiagramme.prototype.drawYAxis = function(){
+			// TODO: Récupérer la couleur dynamiquement à partir du css.
+			var context = this.canvas.getContext('2d');
+			context.strokeStyle = "black";
+			context.beginPath();
+				// Ligne des ordonnées
+				context.moveTo(50, 20);
+				context.lineTo(50, this.getHeight() - 50);
+			context.closePath();
+			context.stroke();
+			
+			// Dessin des intervalles en y
+			var top = data.getTopValue();
+			var nbIntervals = 10; // TODO: a fixer plus tard
+			var lengthInterval = (this.getHeight() - 50 - 20) / nbIntervals;
+			var dataInterval = Math.round(top / nbIntervals);
+			context.beginPath();
+				for (var y = 20; y < this.getHeight() - 2 * lengthInterval; y += lengthInterval) {
+					context.moveTo(50 - 3, y);
+					context.lineTo(50 + 3, y);
+					var textWidth = context.measureText(top).width;
+					context.fillText(top, 50 - textWidth - 5, y + 3, textWidth);
+					top -= dataInterval;
+				}
+			context.closePath();
+			context.stroke();
+		};
+		
+		/**
+		 *	Dessine le diagramme. 
+		 */
+		IDiagramme.prototype.drawDiagram = function(){};
+		
+		/**
+		 * Dessine les lignes de visée
+		 */
+		IDiagramme.prototype.drawYLines = function() {
+			
+		};
+		
+		IDiagramme.prototype.redraw = function() {
+			this.drawAxis();
+			this.drawYLines();
+			this.drawLegend();
+			this.drawDiagram();
+		};
+    }
+};
