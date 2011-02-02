75b8b66c4c60760d5280fb29a1cee3b395c300d0
thueuxludovic@gmail.com <thueuxludovic@gmail.com@f4aee579-5e8d-ec66-c31c-cac246cfbf6d>
2011-02-02 11:22:53 +0000

    suppression des drawAxis dans IDiagramme ajout dans HistoDiagramme

--- trunk/src/ludovic/HistoDiagramme.js
+++ trunk/src/ludovic/HistoDiagramme.js
@@ -6,6 +6,59 @@
 var HistoDiagramme = function(canvasRef, direction) {
 	IDiagramme.call(this, canvasRef);
 
+	/**
+	 * Dessine les abscisses et ordonnées du diagramme
+	 */
+	IDiagramme.prototype.drawAxis = function() {
+		this.drawXAxis();
+		this.drawYAxis();
+	};
+
+	/**
+	 * Dessine les abscisses du diagramme
+	 */
+	IDiagramme.prototype.drawXAxis = function() {
+		var context = this.canvas.getContext('2d');
+		context.strokeStyle = "black";
+		context.beginPath();
+			// Ligne des abscisses
+			context.moveTo(50, this.getHeight() - 50);
+			context.lineTo(this.getWidth() - 50, this.getHeight() - 50);
+		context.closePath();
+		context.stroke();
+	};
+
+	/**
+	 *	Dessine les ordonnées du diagramme
+	 */
+	IDiagramme.prototype.drawYAxis = function(){
+		// TODO: Récupérer la couleur dynamiquement à partir du css.
+		var context = this.canvas.getContext('2d');
+		context.strokeStyle = "black";
+		context.beginPath();
+			// Ligne des ordonnées
+			context.moveTo(50, 20);
+			context.lineTo(50, this.getHeight() - 50);
+		context.closePath();
+		context.stroke();
+
+		// Dessin des intervalles en y
+		var top = this.data.getTopValue();
+		var nbIntervals = 10; // TODO: a fixer plus tard
+		var lengthInterval = (this.getHeight() - 50 - 20) / nbIntervals;
+		var dataInterval = Math.round(top / nbIntervals);
+		context.beginPath();
+			for (var y = 20; y < this.getHeight() - 2 * lengthInterval; y += lengthInterval) {
+				context.moveTo(50 - 3, y);
+				context.lineTo(50 + 3, y);
+				var textWidth = context.measureText(top).width;
+				context.fillText(top, 50 - textWidth - 5, y + 3, textWidth);
+				top -= dataInterval;
+			}
+		context.closePath();
+		context.stroke();
+	};
+
     this.getWidestText = function(texts) {
         var context = this.canvas.getContext('2d');
         var widest = {text: texts[0], length: context.measureText(texts[0]).width};
@@ -30,35 +83,38 @@ var HistoDiagramme = function(canvasRef, direction) {
 		 * Dessin de l'histogramme.
 		 */
 		HistoDiagramme.prototype.drawDiagram = function() {
+			this.drawAxis();
+
 			var context = this.canvas.getContext('2d');
 			//var width = this.getWidth();
 			var height = this.getHeight();
-			
+
 			var total = this.data.getTotal();
 			var tableau = new Array();
 			var that = this;
-			if (this.dir == 'x') { 
+			if (this.dir == 'x') {
 				$.each(this.data.getYLabels(), function(i, yLabel) {
-					//tableau.push(that.data.getLineTotal(yLabel) / total);
+					//tableau = that.data.getYLabels();
 				});
 			} else {
 				$.each(this.data.getXLabels(), function(i, xLabel) {
-					//tableau.push(that.data.getColumnTotal(xLabel) / total);
+					//tableau = that.data.getXLabels();
 				});
 			}
-			
-			$.each(that.data.getXLabels(), function(i, xlabel) { 
-				$.each(that.data.getYLabels(), function(j, ylabel) { 
+
+			$.each(that.data.getXLabels(), function(i, xlabel) {
+				$.each(that.data.getYLabels(), function(j, ylabel) {
 					var val = that.data.getValueByLabel(xlabel, ylabel);
 					if (val != "undefined") {
-						tableau.push(val);	
+						tableau.push(val);
 					}
 				});
 			});
 
-			
+
 			var colors = new Array("blue", "red", "black", "green", "pink", "orange", "darkgreen");
 
+			tableau = tableau.reverse();
 			var maxTableau = that.data.getTopValue();
 			var nbBar = 0;
 			var i = 0;
@@ -66,7 +122,6 @@ var HistoDiagramme = function(canvasRef, direction) {
 				var element = tableau.pop();
 				context.fillStyle = colors[0];
 				context.fillRect(70 + 20 * nbBar * 2, 450 - element/maxTableau * 450, 20, element/maxTableau * 450);
-				context.fillStyle = "black";
 				nbBar++;
 				i++;
 			}
--- trunk/src/ludovic/IDiagramme.js
+++ trunk/src/ludovic/IDiagramme.js
@@ -1,14 +1,14 @@
 var IDiagramme = function(canvasRef) {
 	this.canvas = canvasRef;
 	this.data = null;
-	
+
 	/**
 	 * Prototypage, toutes les methodes définies ici ne seront pas dupliquées
 	 * à l'instanciation.
 	 */
     if (typeof IDiagramme.initialized == "undefined" ) {
         IDiagramme.initialized = true;
-        
+
 		/**
 		 *	Définit la largeur de la fenêtre du diagramme.
 		 *	@param width Largeur de la fenêtre
@@ -17,14 +17,14 @@ var IDiagramme = function(canvasRef) {
 			this.canvas.setAttribute('width', width);
 			this.redraw();
 		};
-	
+
 		/**
 		 *	Retourne la largeur de la fenêtre du diagramme.
 		 */
 		IDiagramme.prototype.getWidth = function(){
 			return this.canvas.getAttribute('width');
 		};
-	
+
 		/**
 		 *	Définit la hauteur de la fenêtre du diagramme
 		 *	@param height Hauteur de la fenêtre.
@@ -33,14 +33,14 @@ var IDiagramme = function(canvasRef) {
 			this.canvas.setAttribute('height', height);
 			this.redraw();
 		};
-	
+
 		/**
 		 *	Retourne la hauteur de la fenêtre du diagramme
 		 */
 		IDiagramme.prototype.getHeight = function(){
 			return this.canvas.getAttribute('height');
 		};
-		
+
 		/**
 		 * Charge un fichier de style pour le diagramme.
 		 * @param css Url d'un fichier css.
@@ -52,7 +52,7 @@ var IDiagramme = function(canvasRef) {
             link.setAttribute('href', css);
             document.getElementsByTagName('head')[0].appendChild(link);
         };
-	
+
 		/**
 		 * Charge les données du diagramme.
 		 * @param dataMatrix Matrice des données
@@ -60,7 +60,7 @@ var IDiagramme = function(canvasRef) {
 		IDiagramme.prototype.setData = function(dataMatrix) {
 			this.data = dataMatrix;
 		};
-		
+
 		/**
 		 *	Dessine la légende du diagramme
 		 */
@@ -109,75 +109,14 @@ var IDiagramme = function(canvasRef) {
                 }
 			});
         };
-		
+
 		/**
-		 * Dessine les abscisses et ordonnées du diagramme
-		 */
-		IDiagramme.prototype.drawAxis = function() {
-			this.drawXAxis();
-			this.drawYAxis();
-		};
-		
-		/**
-		 * Dessine les abscisses du diagramme
-		 */
-		IDiagramme.prototype.drawXAxis = function() {
-			var context = this.canvas.getContext('2d');
-			context.strokeStyle = "black";
-			context.beginPath();
-				// Ligne des abscisses
-				context.moveTo(50, this.getHeight() - 50);
-				context.lineTo(this.getWidth() - 50, this.getHeight() - 50);
-			context.closePath();
-			context.stroke();
-		};
-		
-		/**
-		 *	Dessine les ordonnées du diagramme
-		 */
-		IDiagramme.prototype.drawYAxis = function(){
-			// TODO: Récupérer la couleur dynamiquement à partir du css.
-			var context = this.canvas.getContext('2d');
-			context.strokeStyle = "black";
-			context.beginPath();
-				// Ligne des ordonnées
-				context.moveTo(50, 20);
-				context.lineTo(50, this.getHeight() - 50);
-			context.closePath();
-			context.stroke();
-			
-			// Dessin des intervalles en y
-			var top = this.data.getTopValue();
-			var nbIntervals = 10; // TODO: a fixer plus tard
-			var lengthInterval = (this.getHeight() - 50 - 20) / nbIntervals;
-			var dataInterval = Math.round(top / nbIntervals);
-			context.beginPath();
-				for (var y = 20; y < this.getHeight() - 2 * lengthInterval; y += lengthInterval) {
-					context.moveTo(50 - 3, y);
-					context.lineTo(50 + 3, y);
-					var textWidth = context.measureText(top).width;
-					context.fillText(top, 50 - textWidth - 5, y + 3, textWidth);
-					top -= dataInterval;
-				}
-			context.closePath();
-			context.stroke();
-		};
-		
-		/**
-		 *	Dessine le diagramme. 
+		 *	Dessine le diagramme.
 		 */
 		IDiagramme.prototype.drawDiagram = function(){};
-		
-		/**
-		 * Dessine les lignes de visée
-		 */
-		IDiagramme.prototype.drawYLines = function() {
-			
-		};
-		
+
+
 		IDiagramme.prototype.redraw = function() {
-			this.drawAxis();
-			this.drawYLines();
 			this.drawDiagram();
             this.drawLegend();
 		};
