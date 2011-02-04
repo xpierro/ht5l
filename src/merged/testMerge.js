9418c7938b3679724c793a6095ad6e2db45962e9
xpierro <xpierro@gmail.com>
2011-02-04 17:36:04 +0000

    Version 1.0BETA

--- src/merged/testMerge.js
+++ src/merged/testMerge.js
@@ -1,32 +1,44 @@
 var m = new DataMatrix();
-m.setColumnLabels(new Array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
-m.setRowLabels(new Array("CatsWhoCode.com", "WpRecipes.com", "CatsWhoBlog.com"));
-m.setValue("CatsWhoCode.com", "Monday", 12541);
-m.setValue("CatsWhoCode.com", "Tuesday", 11204);
-m.setValue("CatsWhoCode.com", "Wednesday", 11354);
-m.setValue("CatsWhoCode.com", "Thursday", 10058);
-m.setValue("CatsWhoCode.com", "Friday", 9871);
-m.setValue("CatsWhoCode.com", "Saturday", 8254);
-m.setValue("CatsWhoCode.com", "Sunday", 5477);
+m.setColumnLabels(new Array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"));
+m.setRowLabels(new Array("Clubic.fr", "Google.fr", "Yahoo.fr"));
+m.setValue("Clubic.fr", "Lundi", 12541);
+m.setValue("Clubic.fr", "Mardi", 11204);
+m.setValue("Clubic.fr", "Mercredi", 11354);
+m.setValue("Clubic.fr", "Jeudi", 10058);
+m.setValue("Clubic.fr", "Vendredi", 9871);
+m.setValue("Clubic.fr", "Samedi", 8254);
+m.setValue("Clubic.fr", "Dimanche", 5477);
 
-m.setValue("WpRecipes.com", "Monday", 9855);
-m.setValue("WpRecipes.com", "Tuesday", 8870);
-m.setValue("WpRecipes.com", "Wednesday", 8731);
-m.setValue("WpRecipes.com", "Thursday", 7488);
-m.setValue("WpRecipes.com", "Friday", 8159);
-m.setValue("WpRecipes.com", "Saturday", 6547);
-m.setValue("WpRecipes.com", "Sunday", 4512);
+m.setValue("Google.fr", "Lundi", 9855);
+m.setValue("Google.fr", "Mardi", 8870);
+m.setValue("Google.fr", "Mercredi", 8731);
+m.setValue("Google.fr", "Jeudi", 7488);
+m.setValue("Google.fr", "Vendredi", 8159);
+m.setValue("Google.fr", "Samedi", 6547);
+m.setValue("Google.fr", "Dimanche", 4512);
 
-m.setValue("CatsWhoBlog.com", "Monday", 3241);
-m.setValue("CatsWhoBlog.com", "Tuesday", 2544);
-m.setValue("CatsWhoBlog.com", "Wednesday", 2597);
-m.setValue("CatsWhoBlog.com", "Thursday", 3108);
-m.setValue("CatsWhoBlog.com", "Friday", 2114);
-m.setValue("CatsWhoBlog.com", "Saturday", 2045);
-m.setValue("CatsWhoBlog.com", "Sunday", 950);
+m.setValue("Yahoo.fr", "Lundi", 3241);
+m.setValue("Yahoo.fr", "Mardi", 2544);
+m.setValue("Yahoo.fr", "Mercredi", 2597);
+m.setValue("Yahoo.fr", "Jeudi", 3108);
+m.setValue("Yahoo.fr", "Vendredi", 2114);
+m.setValue("Yahoo.fr", "Samedi", 2045);
+m.setValue("Yahoo.fr", "Dimanche", 950);
 
-var diag = new HistoDiagram(document.getElementsByTagName('canvas')[0], 'column');
-diag.setData(m);
+/*var diag1 = new HistoDiagram(document.getElementsByTagName('canvas')[0], 'column');
+diag1.setData(m);
+var diag2 = new HistoDiagram(document.getElementsByTagName('canvas')[1], 'row');
+diag2.setData(m);
+var diag3 = new PieDiagram(document.getElementsByTagName('canvas')[2], 'column');
+diag3.setData(m);
+var diag4 = new PieDiagram(document.getElementsByTagName('canvas')[3], 'row');
+diag4.setData(m);
+*/
+var ids = new InternalDataSource('testpre');
+ids.loadData(function() {
+    var diag5 = new HistoDiagram(document.getElementsByTagName('canvas')[0], 'row');
+    diag5.setData(ids.getDataMatrix());
+});
 
 /*
 $.each(m.getRowLabels(), function(i, r) {
