14db17e76c704068593afc12f1c1a5548ba6d720
eldjiya <eldjiya@gmail.com>
2011-03-18 08:48:53 +0000

    test verification commit

--- src/abdourahmane/DataMatrix.js
+++ src/abdourahmane/DataMatrix.js
@@ -1,210 +1,202 @@
 /**
- * Matrice des données représentant un tableau à double entrée, indexé en
- * x et en y par des labels.
+ * Matrice des données représentant un tableau à double entrée indexé par les labels de lignes et colonne
+ *             |LabelColonne1|LabelColonne2|
+ * ____________|_____________|______________
+ * LabelLigne1 |Valeur1......|Valeur2......|
+ * ____________|_____________|______________
+ * LabelLigne2 |Valeur3......|Valeur4......|
+ *
+ * Le sens de parcour se fait "en ligne" ou en "colonne". Parcourir en ligne signifie qu'on considère une valeur
+ * comme le resultat d'une fonction d'acces de [LabelLigne][LabelColonne] tandis que parcourir en colonne est inverse:
+ * [LabelColonne][LabelLigne].
+ *
+ * Conception: Pierre Collignon
+ * Commentaires pour corrections: Abdourahmane Djigo et Lyes Kimouche
  */
 
 var DataMatrix = function() {
-	var width = 0;
-	var height = 0;
-	
-	var xLabels = new Array();
-	var yLabels = new Array();
-	
-	var rows = new Array();
-	
+    // Attributs privés
+	this.rowLabels = new Array();
+	this.columnLabels = new Array();
+
+	this.rows = new Array();
+
 	/**
 	 * Prototypage, toutes les methodes définies ici ne seront pas dupliquées
 	 * à l'instanciation.
 	 */
     if (typeof DataMatrix.initialized == "undefined" ) {
         DataMatrix.initialized = true;
-    	/**
-    	 * Retourne l'index correspondant à la valeur
-    	 * @param value La valeur à retrouver
-    	 * @returns L'index de la valeur
-    	 */
-    	DataMatrix.prototype.findValueInArray = function(array, value) {
-    		var index = -1;
-    		$.each(array, function(i, val) {
-    			if (value == val) {
-    				index = i;
-    				return false;
-    			}
-                return true;
-    		});
-    		return index;
-    	};
-    	
+
     	/**
     	 * Retourne la largeur de la matrice.
     	 */
-    	DataMatrix.prototype.getWidth = function() {
-    		return width;
+    	DataMatrix.prototype.getColumnNumber = function() {
+    		return this.columnLabels.length;
     	};
-    	
+
     	/**
     	 * Retourne la hauteur de la matrice.
     	 */
-    	DataMatrix.prototype.getHeight = function() {
-    		return height;
+    	DataMatrix.prototype.getRowNumber = function() {
+    		return this.rowLabels.length;
     	};
-    	
+
     	/**
-    	 * Retourne les labels en x.
+    	 * Retourne les labels de colonne.
     	 */
-    	DataMatrix.prototype.getXLabels = function() {
-    		return xLabels;
+    	DataMatrix.prototype.getColumnLabels = function() {
+    		return this.columnLabels;
     	};
-    	
+
     	/**
-    	 * Retourne les labels en y.
+    	 * Retourne les labels en line.
     	 */
-    	DataMatrix.prototype.getYLabels = function() {
-    		return yLabels;
+    	DataMatrix.prototype.getRowLabels = function() {
+    		return this.rowLabels;
     	};
-    	
+
     	/**
-    	 * Renvoie la somme de tous les éléments du tableau.
+    	 * Renvoie la somme de tous les éléments du tableau à double entrée.
     	 */
         DataMatrix.prototype.getTotal = function() {
     		var total = 0;
-    		for (var x = 0; x < width; x++) {
-    			for (var y = 0; y < height; y++) {
-    				total += this.getValue(x, y);
-    			}
-    		}
+    		$.each(this.rowLabels, $.proxy(function(rowIndex, rowLabel) {
+                $.each(this.columnLabels, $.proxy(function(columnIndex, columnLabel) {
+                    total += this.getValueByLabel(rowLabel, columnLabel);
+                }, this));
+            }, this));
     		return total;
     	};
 
     	/**
-    	 * Spécifie le nom de chaque abscisse
+    	 * Spécifie le nom de chaque colonne
     	 */
-    	DataMatrix.prototype.setXAxisLabels = function(labels) {
-    		$.each(labels, function(index, value) {
-    			xLabels.push(value);
-    		});
-    		width = xLabels.length;
+    	DataMatrix.prototype.setColumnLabels = function(labels) {
+    		$.each(labels, $.proxy(function(index, value) {
+    			this.columnLabels.push(value);
+    		}, this));
     	};
-    	
+
     	/**
-    	 * Spécifie le nom de chaque ordonnée
-    	 */	
-    	DataMatrix.prototype.setYAxisLabels = function(labels) {
-    		$.each(labels, function(index, value) {
-    			yLabels.push(value);
-    		});
-    		height = yLabels.length;
-    	};
-    	
-    	/**
-    	 * Ajoute un label d'abscisse.
+    	 * Spécifie le nom de chaque ligne
     	 */
-    	DataMatrix.prototype.addXAxisLabel = function(label) {
-    		xLabels.push(label);
-    		width += 1;
+    	DataMatrix.prototype.setRowLabels = function(labels) {
+    		$.each(labels, $.proxy(function(index, value) {
+    			this.rowLabels.push(value);
+    		}, this));
     	};
-    	
+
     	/**
-    	 * Ajoute un label d'ordonnée.
+    	 * Ajoute un label de colonne.
     	 */
-    	DataMatrix.prototype.addYAxisLabel = function(label) {
-    		yLabels.push(label);
-    		height += 1;
+    	DataMatrix.prototype.addColumnLabel = function(label) {
+    		this.columnLabels.push(label);
     	};
-    	
+
     	/**
-    	 * Insère la valeur value dans la matrices, aux coordonnées labélisées
+    	 * Ajoute un label de ligne.
+    	 */
+    	DataMatrix.prototype.addRowLabel = function(label) {
+    		this.rowLabels.push(label);
+    	};
+
+    	/**
+    	 * Insère la valeur value dans la matrice, aux coordonnées labélisées
     	 * fournies.
     	 */
-    	DataMatrix.prototype.setValue = function(xLabel, yLabel, value) {
-    		var i = this.findValueInArray(xLabels, xLabel);
-    		var j = this.findValueInArray(yLabels, yLabel);
-    		if (i != -1 && j != -1) {
-    			if (!rows[yLabel]) {
-    				rows[yLabel] = new Array();
+    	DataMatrix.prototype.setValue = function(rowLabel, columnLabel, value) {
+    		if ($.inArray(rowLabel, this.rowLabels) != -1 && $.inArray(columnLabel, this.columnLabels) != -1) {
+    			if (!this.rows[rowLabel]) {
+    				this.rows[rowLabel] = new Array();
     			}
-    			rows[yLabel][xLabel] = value;
+    			this.rows[rowLabel][columnLabel] = value;
     		} else {
-    			throw "Label indéfini";
+    			throw "Labels indéfinis : " + rowLabel + "," + columnLabel;
     		}
     	};
-    	
+
     	/**
-    	 * Retoune la valeur entrée dans la matrice selon les labels en x et y.
+    	 * Retoune la valeur entrée dans la matrice selon les labels.
     	 */
-    	DataMatrix.prototype.getValueByLabel = function(xLabel, yLabel) {
-    		if (!rows[yLabel] || !rows[yLabel][xLabel]) {
-    			throw "Label/Valeur indéfinis : [" + xLabel + ";" + yLabel + "]";
+    	DataMatrix.prototype.getValueByLabel = function(rowLabel, columnLabel) {
+    		if (!this.rows[rowLabel] || !this.rows[rowLabel][columnLabel]) {
+    			throw "Label/Valeur indéfinis : " + rowLabel + "," + columnLabel;
     		}
-    		return rows[yLabel][xLabel];
+    		return this.rows[rowLabel][columnLabel];
     	};
-    	
-    	/**
-    	 * Retoune la valeur entrée dans la matrice selon les labels en x et y selon
+
+        /**
+    	 * Retoune la valeur entrée dans la matrice selon les labels de ligne et colonne, selon
     	 * la direction.
+         * @param firstLabel Label de ligne si dir == 'row' | Label de colonne si dir == 'column'
+         * @param secondLabel Label de colonne si dir == 'row' | Label de ligne si dir == 'column'
     	 */
-    	DataMatrix.prototype.getValueByLabelAndDirection = function(xLabel, yLabel, dir) {
-    		if (!rows[yLabel] && !rows[xLabel]) {
-    			throw "Label/Valeur indéfinis : [" + xLabel + ";" + yLabel + "]";
+    	DataMatrix.prototype.getValueByLabelAndDirection = function(firstLabel, secondLabel, dir) {
+    		if (!this.rows[firstLabel] && !this.rows[secondLabel]) {
+    			throw "Label/Valeur indéfinis : " + firstLabel + ";" + secondLabel;
     		}
-    		if (dir == 'y') {
-    			return rows[yLabel][xLabel];
+    		if (dir == 'row') {
+    			return this.rows[firstLabel][secondLabel];
     		} else {
-    			return rows[xLabel][yLabel];
+    			return this.rows[secondLabel][firstLabel];
     		}
     	};
-    	
+
     	/**
     	 * Retourne la valeur entrée dans la matrice selon ses coordonnées
     	 * numériques.
     	 */
     	DataMatrix.prototype.getValue = function(x, y) {
-    		return this.getValueByLabel(xLabels[x], yLabels[y]);
+    		return this.getValueByLabel(this.rowLabels[x], this.columnLabels[y]);
     	};
-    	
+
     	/**
     	 * Retourne la valeur maximale contenu dans la matrice
     	 */
     	DataMatrix.prototype.getTopValue = function() {
-    		var top = this.getValue(0, 0);
-    		for (var x = 0; x < width; x++) {
-    			for (var y = 0; y < height; y++) {
-    				var value = this.getValue(x, y);
-    				if (value > top) {
-    					top = value;
-    				}
-    			}
-    		}
+    		var top;
+            try {
+                top = this.getValue(0, 0);
+            } catch(e) {
+                top = 0;
+            }
+            $.each(this.rowLabels, $.proxy(function(rowIndex, rowLabel) {
+                $.each(this.columnLabels, $.proxy(function(columnIndex, columnLabel) {
+                    var currentValue = this.getValueByLabel(rowLabel, columnLabel);
+                    if (currentValue > top) {
+                        top = currentValue;
+                    }
+                }, this));
+            }, this));
     		return top;
     	};
-    	
+
     	/**
-    	 * Retourne la somme des valeurs d'une ligne, indéxée par un label en y.
+    	 * Retourne la somme des valeurs d'une ligne.
     	 */
-    	DataMatrix.prototype.getLineTotal = function(yLabel) {
-    		if (this.findValueInArray(yLabels, yLabel) < 0) {
-    			throw "Label inexistant : " + yLabel;
-    		}
+    	DataMatrix.prototype.getRowTotal = function(rowLabel) {
+            if (!this.rows[rowLabel]) {
+                throw "Label inexistant : " + rowLabel;
+            }
     		var sum = 0;
-    		var ref = this;
-    		$.each(xLabels, function(i, xLabel) {
-    			sum += ref.getValueByLabel(xLabel, yLabel);
-    		});
+    		$.each(this.columnLabels, $.proxy(function(i, columnLabel) {
+    			sum += this.getValueByLabel(rowLabel, columnLabel);
+    		}, this));
     		return sum;
     	};
-    	
+
     	/**
-    	 * Retourne la somme des valeurs d'une colonne indéxée par un  label en x.
+    	 * Retourne la somme des valeurs d'une colonne.
     	 */
-    	DataMatrix.prototype.getColumnTotal = function(xLabel) {
-    		if (this.findValueInArray(xLabels, xLabel) < 0) {
-    			throw "Label inexistant : " + xLabel;
-    		}
+    	DataMatrix.prototype.getColumnTotal = function(columnLabel) {
+    		if (!this.rows[this.rowLabels[0]][columnLabel]) {
+                throw "Label inexistant : " + columnLabel;
+            }
     		var sum = 0;
-    		var ref = this;
-    		$.each(yLabels, function(i, yLabel) {
-    			sum += ref.getValueByLabel(xLabel, yLabel);
-    		});
+    		$.each(this.rowLabels, $.proxy(function(i, rowLabel) {
+    			sum += this.getValueByLabel(rowLabel, columnLabel);
+    		}, this));
     		return sum;
     	};
     }
