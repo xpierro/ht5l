8426f0ed1ad19f0072006e5e09ed1af0e9f74e5a
geglou@gmail.com <geglou@gmail.com@f4aee579-5e8d-ec66-c31c-cac246cfbf6d>
2011-05-16 23:32:38 +0000

    Infobulle pour le pie faite (bien sûr, on peut modifier l'apparence, j'ai des goûts de chiotte) j'ai crée un sous-dossier dans mon dossier car le pie réactif d'Abdou ne voulait pas marcher dans mon dossier principal (pas réussi a trouver pourquoi)

---
+++ trunk/src/maxence/testMerge.js
@@ -0,0 +1,56 @@
+var m = new DataMatrix();
+m.setColumnLabels(new Array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"));
+m.setRowLabels(new Array("Clubic.fr", "Google.fr", "Yahoo.fr"));
+m.setValue("Clubic.fr", "Lundi", 12541);
+m.setValue("Clubic.fr", "Mardi", 10533);
+m.setValue("Clubic.fr", "Mercredi", 11400);
+m.setValue("Clubic.fr", "Jeudi", 10058);
+m.setValue("Clubic.fr", "Vendredi", 11287);
+m.setValue("Clubic.fr", "Samedi", 8254);
+m.setValue("Clubic.fr", "Dimanche", 9361);
+
+
+
+m.setValue("Google.fr", "Lundi", 9855);
+m.setValue("Google.fr", "Mardi", 7525);
+m.setValue("Google.fr", "Mercredi", 8731);
+m.setValue("Google.fr", "Jeudi", 7488);
+m.setValue("Google.fr", "Vendredi", 8159);
+m.setValue("Google.fr", "Samedi", 6547);
+m.setValue("Google.fr", "Dimanche", 7512);
+
+
+m.setValue("Yahoo.fr", "Lundi", 1255);
+m.setValue("Yahoo.fr", "Mardi", 2544);
+m.setValue("Yahoo.fr", "Mercredi", 1694);
+m.setValue("Yahoo.fr", "Jeudi", 3108);
+m.setValue("Yahoo.fr", "Vendredi", 800);
+m.setValue("Yahoo.fr", "Samedi", 2045);
+m.setValue("Yahoo.fr", "Dimanche", 950);
+
+//var diag1 = new LineDiagram(document.getElementsByTagName('canvas')[0], 'row');
+var diag1 = new PieDiagram(document.getElementsByTagName('canvas')[0], 'row');
+document.getElementsByTagName('canvas')[0].onmousemove = function(event) {
+	diag1.handleClick(event, diag1);
+};
+diag1.setData(m);
+
+/*
+var diag1 = new HistoDiagram(document.getElementsByTagName('canvas')[0], 'column');
+diag1.setData(m);
+var diag2 = new HistoDiagram(document.getElementsByTagName('canvas')[1], 'row');
+diag2.setData(m);
+var diag3 = new PieDiagram(document.getElementsByTagName('canvas')[2], 'column');
+diag3.setData(m);
+var diag4 = new PieDiagram(document.getElementsByTagName('canvas')[3], 'row');
+diag4.setData(m);
+*/
+
+/*
+$.each(m.getRowLabels(), function(i, r) {
+    $.each(m.getColumnLabels(), function(j, c) {
+        alert(r + " | " + c + " | " + m.getValueByLabel(r, c));
+    });
+});
+alert(m.getTopValue());
+*/
