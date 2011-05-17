b0797b48b5eb347717f8f95370b2f875c186351e
xpierro@gmail.com <xpierro@gmail.com@f4aee579-5e8d-ec66-c31c-cac246cfbf6d>
2011-05-17 14:23:07 +0000

    Changement des couleurs de l'Histogramme

--- trunk/src/pierre/testMerge.js
+++ trunk/src/pierre/testMerge.js
@@ -1,22 +1,36 @@
-var diag3DInt = new Histo3DDiagram(document.getElementsByTagName('canvas')[0], 'row');
-//var diag3DExt = new Histo3DDiagram(document.getElementById('test3DExt'), 'column');
-var diagPieInt = new PieDiagram(document.getElementsByTagName('canvas')[1], 'row');
-//var diagPieExt = new PieDiagram(document.getElementById('testPieExt'), 'column');
-var diagHistInt = new HistoDiagram(document.getElementsByTagName('canvas')[2], 'row');
-//var diagHistExt = new HistoDiagram(document.getElementById('testHistExt'), 'column');
-var diagLineInt = new LineDiagram(document.getElementsByTagName('canvas')[3], 'column');
-//var diagLineExt = new LineDiagram(document.getElementById('testLineExt'), 'column');
+var m = new DataMatrix();
+m.setColumnLabels(new Array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"));
+m.setRowLabels(new Array("Clubic.fr", "Google.fr", "Yahoo.fr"));
+m.setValue("Clubic.fr", "Lundi", 12541);
+m.setValue("Clubic.fr", "Mardi", 10533);
+m.setValue("Clubic.fr", "Mercredi", 11400);
+m.setValue("Clubic.fr", "Jeudi", 10058);
+m.setValue("Clubic.fr", "Vendredi", 11287);
+m.setValue("Clubic.fr", "Samedi", 8254);
+m.setValue("Clubic.fr", "Dimanche", 9361);
 
-var internalDiagrams = new Array(diag3DInt, diagPieInt, diagHistInt, diagLineInt);
 
-var ids = new InternalDataSource('testpre');
-var iss = new InternalStyleSource('testpre');
 
-ids.loadData(function() {
-    iss.loadData(function() {
-        $.each(internalDiagrams, function(i, diag) {
-            diag.setData(ids.getDataMatrix());
-            diag.setStyle(iss);
-        });
-    });
-});
+m.setValue("Google.fr", "Lundi", 9855);
+m.setValue("Google.fr", "Mardi", 7525);
+m.setValue("Google.fr", "Mercredi", 8731);
+m.setValue("Google.fr", "Jeudi", 7488);
+m.setValue("Google.fr", "Vendredi", 8159);
+m.setValue("Google.fr", "Samedi", 6547);
+m.setValue("Google.fr", "Dimanche", 7512);
+
+
+m.setValue("Yahoo.fr", "Lundi", 1255);
+m.setValue("Yahoo.fr", "Mardi", 2544);
+m.setValue("Yahoo.fr", "Mercredi", 1694);
+m.setValue("Yahoo.fr", "Jeudi", 3108);
+m.setValue("Yahoo.fr", "Vendredi", 800);
+m.setValue("Yahoo.fr", "Samedi", 2045);
+m.setValue("Yahoo.fr", "Dimanche", 950);
+
+// TODO: intégrer au diagramme directement la gestion du mousemove
+var diag1 = new HistoDiagram(document.getElementsByTagName('canvas')[0], 'row');
+document.getElementsByTagName('canvas')[0].onmousemove = function(event) {
+	diag1.handleClick(event, diag1);
+};
+diag1.setData(m);
