9b23b9e6d02e018de3b94a2bbee5621e2dc9ed69
xpierro <xpierro@gmail.com>
2011-05-17 14:37:30 +0000

    Histogramme en 3D fini

--- src/pierre/Histo3DDiagram.js
+++ src/pierre/Histo3DDiagram.js
@@ -10,6 +10,7 @@ var Histo3DDiagram = function(canvasRef, direction) {
 		throw "Direction de lecture invalide 3D : " + direction;
 	}
 	this.dir = direction;
+    this.currentSlice = null;
 	if (typeof Histo3DDiagram.initialized == "undefined") {
 		Histo3DDiagram.initialized = true;
 
@@ -95,6 +96,16 @@ var Histo3DDiagram = function(canvasRef, direction) {
 				$.each(colorLabels, $.proxy(function(j, colorlabel) {
 					var value = this.data.getValueByLabelAndDirection(colorlabel, abslabel, this.dir);
 					var color = this.getColors()[j];
+
+                    if (this.currentSlice != null) {
+                        if(this.currentSlice.abs == abslabel && this.currentSlice.color == colorlabel) {
+                            color = this.applyAlphaToColor(this.getColors()[j < this.getColors().length ? j : j % this.getColors().length], .5);
+                        } else {
+                            color = this.applyAlphaToColor(this.getColors()[j < this.getColors().length ? j : j % this.getColors().length], 1);
+                        }
+                    }
+
+
 					var barHeight = this.getPixelPerUnit() * value;
 					context.fillStyle = color;
                     context.strokeStyle = 'black';
@@ -142,6 +153,78 @@ var Histo3DDiagram = function(canvasRef, direction) {
 				context.fillText(abslabel, xLegendPosition , this.getHeight() - this.getBottomShift() + 10);
 				currentX += shift;
 			}, this));
+        };
+
+        Histo3DDiagram.prototype.handleClick = function(clickEvent, that) {
+            var mouseX = clickEvent.pageX - this.canvas.offsetLeft;
+            var mouseY = clickEvent.pageY - this.canvas.offsetTop;
+
+            var context = this.canvas.getContext('2d');
+
+			var shift = 5; // Décalage entre deux ensembles en abscisse
+            var firstShift = 20; // Premier décalage
+			var currentX = this.getLeftShift() + firstShift; // Position en x sur le canvas du pinceau
+
+			// Calcul des ensemble servant d'abscisse et de couleur selon la direction de parcours
+			// du tableau de données
+            var absLabels = null;
+            var colorLabels = null;
+			if (this.dir == 'column') {
+				absLabels = this.data.getRowLabels();
+				colorLabels = this.data.getColumnLabels();
+			} else {
+				absLabels = this.data.getColumnLabels();
+				colorLabels = this.data.getRowLabels();
+			}
+
+			var globalShift = (absLabels.length - 1) * shift; // Somme des décalages entre deux ensembles en abscisse
+
+			// Largeur d'une barre
+			var barWidth = ((this.getWidth() - currentX   - globalShift)
+                            / (this.data.getColumnNumber() * this.data.getRowNumber()));
+            var yZero = 0;
+            if (this.data.getTopValue() < 0 || this.data.getBottomValue() < 0) {
+                yZero = (this.getHeight() - this.yAxisConfig.bottomShift + this.yAxisConfig.topShift) / 2;
+            } else {
+                yZero = this.getHeight() - this.getBottomShift();
+            }
+
+            var found = 0;
+			$.each(absLabels, $.proxy(function(i, abslabel){
+				$.each(colorLabels, $.proxy(function(j, colorlabel) {
+					var value = this.data.getValueByLabelAndDirection(colorlabel, abslabel, this.dir);
+					var barHeight = this.getPixelPerUnit() * value;
+					if (mouseX >= currentX && mouseX <= currentX + barWidth
+                        && mouseY >= yZero - barHeight && mouseY <= yZero) {
+                        that.currentSlice = {abs: abslabel, color: colorlabel};
+                        that.redraw();
+                        found = 1;
+                    }
+					currentX += barWidth;
+				}, this));
+				currentX += shift;
+			}, this));
+
+            if (that.currentSlice != null && found == 0) {
+                that.currentSlice = null;
+                that.redraw();
+            }
+        };
+
+        Histo3DDiagram.prototype.getRGBFromName = function(name) {
+            var div = document.createElement("div");
+            div.style.color = name;
+            document.body.appendChild(div);
+            var rgb = window.getComputedStyle(div, null).color;
+            document.body.removeChild(div);
+                    return rgb;
+        };
+
+        Histo3DDiagram.prototype.applyAlphaToColor = function(name, alpha) {
+            var rgb = this.getRGBFromName(name);
+            var rgba = rgb.replace('rgb', 'rgba');
+            rgba = rgba.replace(')', ', ' + alpha + ')');
+            return rgba;
         };
     }
 };
