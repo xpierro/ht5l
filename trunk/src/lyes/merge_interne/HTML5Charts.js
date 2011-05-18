5bb8ab66c8e1370ef89a7b3b4e6fc192a91979fc
thueuxludovic <thueuxludovic@f4aee579-5e8d-ec66-c31c-cac246cfbf6d>
2011-05-18 16:28:13 +0000

    merge final sans comm ^^

---
+++ trunk/src/lyes/merge_interne/HTML5Charts.js
@@ -0,0 +1,28 @@
+/**
+ * Charge tous les scripts n�cessaire au fonctionnement de la biblioth?que
+ * Conception: Pierre Collignon.
+ */
+
+// On d�finit les fichier:
+var jsFiles = new Array("DataMatrix.js", "IStyleSource.js", "InternalStyleSource.js", "ExternalStyleSource.js",
+		"StyleMatrix.js", "IDataSource.js", "InternalDataSource.js","ExternalDataSource.js","ConnectorDataSource.js",
+		"IDiagram.js", "PieDiagram.js", "HistoDiagram.js", "LineDiagram.js","Histo3DDiagram.js", "window_alert.js",
+		"testMerge.js");
+
+/**
+ * Charge un script
+ * 
+ * @param url
+ *            Url du script
+ */
+var loadScript = function(url) {
+	var script = document.createElement('script');
+	script.setAttribute('type', 'text/javascript');
+	script.setAttribute('src', url);
+	document.getElementsByTagName('head')[0].appendChild(script);
+};
+
+// On charge tout (sans utiliser JQuery qu'on ins?re nous meme)
+for ( var i = 0; i < jsFiles.length; i++) {
+	loadScript(jsFiles[i]);
+}
