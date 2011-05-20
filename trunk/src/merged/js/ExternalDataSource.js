5347759a953d5eb46363e7b7311ee92b649443af
thueuxludovic <thueuxludovic@f4aee579-5e8d-ec66-c31c-cac246cfbf6d>
2011-05-20 15:17:49 +0000

    gestion des exceptions terminée

--- trunk/src/merged/js/ExternalDataSource.js
+++ trunk/src/merged/js/ExternalDataSource.js
@@ -62,37 +62,55 @@ var ExternalDataSource = function(url) {
          * Renvoie la matrice représentant les données lues.
          */
         ExternalDataSource.prototype.getDataMatrix = function(xmlResponse) {
-            var dataMatrix = new DataMatrix();
-            
-            var yLeg = xmlResponse.getElementsByTagName('ylegend')[0];
-            dataMatrix.setYLegend(yLeg.textContent);
         	
-            var series =  xmlResponse.getElementsByTagName('series')[0];
-            $.each(series.childNodes, function(i, childNode) {
-                if (childNode.tagName == 'serie') {
-                	if(childNode.attributes.getNamedItem('name') == null || childNode.attributes.getNamedItem('name').value == ""){
-                		throw "nom de la serie non defini";
-                	}
-                    dataMatrix.addColumnLabel(childNode.attributes.getNamedItem('name').value);
-                    $.each(childNode.childNodes, function(j, grandChildNode) {                   
-                        if (grandChildNode.tagName == 'value') {
-                            if (!dataMatrix.hasRowLabel(grandChildNode.attributes.getNamedItem('label').value)) {
+        	try {
+	            var dataMatrix = new DataMatrix();
+	            
+	            if(xmlResponse.getElementsByTagName('ylegend')[0] == null) {
+	            	throw 'La légende en Y n\'est pas spécifiée';
+	            }
+	            var yLeg = xmlResponse.getElementsByTagName('ylegend')[0];
+	            dataMatrix.setYLegend(yLeg.textContent);
+	        	
+	            var series =  xmlResponse.getElementsByTagName('series')[0];
+	            if (series.childNodes.length == 1) {
+            		throw "Le nombre de séries ne doit pas être nul";
+            	}
+	            $.each(series.childNodes, function(i, childNode) {
+	                if (childNode.tagName == 'serie') {
+	                	if(childNode.attributes.getNamedItem('name') == null || childNode.attributes.getNamedItem('name').value == ""){
+	                		throw 'Le nom de la série n\'est pas spécifié';
+	                	}
+	                    dataMatrix.addColumnLabel(childNode.attributes.getNamedItem('name').value);
+	                    if (childNode.childNodes.length == 1) {
+                    		throw "Le nombre de valeurs ne doit pas être nul";
+                    	}
+	                    $.each(childNode.childNodes, function(j, grandChildNode) {                   
+	                        if (grandChildNode.tagName == 'value') {
                             	if(grandChildNode.attributes.getNamedItem('label') == null || grandChildNode.attributes.getNamedItem('label').value == ""){
-                            		throw "nom de la colomne non defini";
+                            		throw 'Le label est mal formé';
                             	}
-                                dataMatrix.addRowLabel(grandChildNode.attributes.getNamedItem('label').value);
-                            }
-                            if(isNaN(parseInt(grandChildNode.textContent))){
-                        		throw "valeur non defini";
-                        	}
-                            dataMatrix.setValue(dataMatrix.getRowLabels()[(j - 1) / 2],
-                                                dataMatrix.getColumnLabels()[(i - 1) / 2],
-                                                parseInt(grandChildNode.textContent));
-                        } 
-                    });  
-                }
-            });
-            return dataMatrix;
+		                        if (!dataMatrix.hasRowLabel(grandChildNode.attributes.getNamedItem('label').value)) {
+	                                dataMatrix.addRowLabel(grandChildNode.attributes.getNamedItem('label').value);
+	                            }
+	                            if(grandChildNode.textContent == "" || isNaN(parseInt(grandChildNode.textContent))){
+	                        		throw 'La valeur est mal formatée';
+	                        	}
+	                            dataMatrix.setValue(dataMatrix.getRowLabels()[(j - 1) / 2],
+	                                                dataMatrix.getColumnLabels()[(i - 1) / 2],
+	                                                parseInt(grandChildNode.textContent));
+	                        } else if (grandChildNode.tagName != undefined){
+			                	throw "La série est mal formée";
+			                }
+	                    });  
+	                } else if (childNode.tagName != undefined){
+	                	throw "La série est mal formée";
+	                }
+	            });
+	            return dataMatrix;
+        	} catch(e) {
+	    	   window_alert('XML non conforme', e);
+	       }
         }
     }
 };
